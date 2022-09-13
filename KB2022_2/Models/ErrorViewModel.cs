092fcda9d7cebeb3a9d215e0a9a3bbec399e2884
Hereigo <hereigo@online.ua>
2022-09-13 11:38:21 +0300

    add asp.net 6.0

---
+++ KB2022_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KB2022_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
