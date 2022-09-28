cdf4089de12fb5fdd810fdfe13e1cdafc004981e
Hereigo <hereigo@online.ua>
2022-09-28 20:50:10 +0300

    sql insert by condition time-slot

--- Database1/1_Begin.sql
+++ Database1/1_Begin.sql
@@ -19,29 +19,55 @@ CREATE TRIGGER TR_MY_TRIG on dbo.Table1 INSTEAD OF INSERT
 AS
 BEGIN
 	IF NOT EXISTS (
-		SELECT 1 FROM inserted i 
-		INNER JOIN dbo.Table1 e
-		ON e.StartedAt=i.StartedAt AND e.MinutesDuration=i.MinutesDuration)
+		SELECT 1 FROM dbo.Table1 e
+		INNER JOIN INSERTED i
+		ON (i.StartedAt <= e.StartedAt AND DATEADD(MINUTE, i.MinutesDuration, i.StartedAt) >= e.StartedAt)
+		OR (i.StartedAt > e.StartedAt AND i.StartedAt < DATEADD(MINUTE, e.MinutesDuration, e.StartedAt))
+	)
 	BEGIN
 		INSERT INTO dbo.Table1
 		(Name, StartedAt, MinutesDuration)
 		SELECT [Name], StartedAt, MinutesDuration
-		FROM inserted
+		FROM INSERTED
 	END
 END
 GO
-
 ----------------------------------------------------
-
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
-
 INSERT INTO dbo.Table1
 (Name, StartedAt, MinutesDuration)
 VALUES
-('Alex', @MyDate, 30),
-('Felix', DATEADD(MINUTE, 10, @MyDate), 30),
-('Maxx', DATEADD(MINUTE, 30, @MyDate), 30),
-('Trisha', DATEADD(MINUTE, 50, @MyDate), 30)
+('Alex', @MyDate, 30)
+GO
+DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
+INSERT INTO dbo.Table1
+(Name, StartedAt, MinutesDuration)
+VALUES
+('Wrongel', DATEADD(MINUTE, 10, @MyDate), 30)
+GO
+DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
+INSERT INTO dbo.Table1
+(Name, StartedAt, MinutesDuration)
+VALUES
+('Maxx', DATEADD(MINUTE, 30, @MyDate), 30)
+GO
+DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
+INSERT INTO dbo.Table1
+(Name, StartedAt, MinutesDuration)
+VALUES
+('Trisha', DATEADD(MINUTE, 65, @MyDate), 30)
+GO
+DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
+INSERT INTO dbo.Table1
+(Name, StartedAt, MinutesDuration)
+VALUES
+('Wrongel2', DATEADD(MINUTE, 30, @MyDate), 30)
+GO
+DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
+INSERT INTO dbo.Table1
+(Name, StartedAt, MinutesDuration)
+VALUES
+('Daisy', DATEADD(MINUTE, 65, @MyDate), 30)
 GO
 
 SELECT * FROM dbo.Table1
