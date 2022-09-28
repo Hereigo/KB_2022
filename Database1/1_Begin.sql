fc36b5288d863d08c3cfe030211ba743754d8506
Hereigo <hereigo@online.ua>
2022-09-28 11:04:42 +0300

    db new proj

---
+++ Database1/1_Begin.sql
@@ -0,0 +1,14 @@
+﻿ USE Database1_1
+ GO
+ 
+ IF OBJECT_ID('[dbo].[Table1]') IS NULL
+CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] VARCHAR NOT NULL,
+	[StartedAt] DATETIME NOT NULL,
+	[MinutesDuration] INT NOT NULL
+)
+GO
+
+ SELECT * FROM Table1
