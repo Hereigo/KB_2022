e1356f6adae72c1c804b732767ebf69643070c61
Hereigo <hereigo@online.ua>
2022-09-29 17:46:44 +0300

    update sql no-intersect trigger

--- Database1/1_Begin.sql
+++ Database1/1_Begin.sql
@@ -13,25 +13,33 @@ CREATE TABLE dbo.Table1
 )
 GO
 
-
--- IF NOT EXISTS(SELECT * FROM SYS.TRIGGERS WHERE NAME='TR_MY_TRIG')
-CREATE TRIGGER TR_MY_TRIG on dbo.Table1 INSTEAD OF INSERT
+-- IF NOT EXISTS(SELECT * FROM SYS.TRIGGERS WHERE NAME='TR_NoIntersectTimeSlot_Table1')
+CREATE TRIGGER TR_NoIntersectTimeSlot_Table1 on dbo.Table1 INSTEAD OF INSERT
 AS
 BEGIN
 	IF NOT EXISTS (
 		SELECT 1 FROM dbo.Table1 e
 		INNER JOIN INSERTED i
-		ON (i.StartedAt <= e.StartedAt AND DATEADD(MINUTE, i.MinutesDuration, i.StartedAt) >= e.StartedAt)
-		OR (i.StartedAt > e.StartedAt AND i.StartedAt < DATEADD(MINUTE, e.MinutesDuration, e.StartedAt))
+		--ON i.StartedAt <= e.StartedAt AND DATEADD(MINUTE, i.MinutesDuration, i.StartedAt) >= e.StartedAt
+		--OR i.StartedAt > e.StartedAt AND i.StartedAt < DATEADD(MINUTE, e.MinutesDuration, e.StartedAt)
+		ON e.StartedAt < DATEADD(MINUTE, i.MinutesDuration, i.StartedAt) 
+			AND DATEADD(MINUTE, e.MinutesDuration, e.StartedAt) > i.StartedAt
 	)
 	BEGIN
 		INSERT INTO dbo.Table1
 		(Name, StartedAt, MinutesDuration)
-		SELECT [Name], StartedAt, MinutesDuration
+		SELECT Name, StartedAt, MinutesDuration
 		FROM INSERTED
+	END
+
+	ELSE
+	BEGIN
+		RAISERROR('Time slot intersect with some other.', 16, 1)
 	END
 END
 GO
+----------------------------------------------------
+-- TEST
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
 INSERT INTO dbo.Table1
@@ -43,7 +51,7 @@ DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
 INSERT INTO dbo.Table1
 (Name, StartedAt, MinutesDuration)
 VALUES
-('Wrongel', DATEADD(MINUTE, 10, @MyDate), 30)
+('Felix', DATEADD(MINUTE, 10, @MyDate), 30)
 GO
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
 INSERT INTO dbo.Table1
@@ -61,13 +69,25 @@ DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
 INSERT INTO dbo.Table1
 (Name, StartedAt, MinutesDuration)
 VALUES
-('Wrongel2', DATEADD(MINUTE, 30, @MyDate), 30)
+('Sandra', DATEADD(MINUTE, 30, @MyDate), 30)
 GO
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
 INSERT INTO dbo.Table1
 (Name, StartedAt, MinutesDuration)
 VALUES
 ('Daisy', DATEADD(MINUTE, 65, @MyDate), 30)
+GO
+DECLARE @MyDate DATETIME = '2022-09-28 12:29:00';
+INSERT INTO dbo.Table1
+(Name, StartedAt, MinutesDuration)
+VALUES
+('Catana', DATEADD(MINUTE, 5, @MyDate), 30)
+GO
+DECLARE @MyDate DATETIME = '2022-09-28 12:31:00';
+INSERT INTO dbo.Table1
+(Name, StartedAt, MinutesDuration)
+VALUES
+('Donald', DATEADD(MINUTE, 5, @MyDate), 30)
 GO
 
 SELECT * FROM dbo.Table1
