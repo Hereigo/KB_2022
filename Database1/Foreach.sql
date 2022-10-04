84de86456771538383f73a8945c011d18a5d9109
Hereigo <hereigo@online.ua>
2022-10-04 20:59:40 +0300

    add sql foreach

---
+++ Database1/Foreach.sql
@@ -0,0 +1,39 @@
+﻿USE Database1_1
+GO
+
+DROP TABLE dbo.TableForTests
+ 
+CREATE TABLE dbo.TableForTests
+(
+	UserId INT NOT NULL IDENTITY PRIMARY KEY,
+	UserName VARCHAR(30) NOT NULL
+)
+GO
+
+INSERT INTO dbo.TableForTests (UserName)
+VALUES
+('Alex'),('Felix'),('Max'),('Sandy'),('Daisy'),('Catana')
+GO
+-- SELECT * FROM dbo.TableForTests
+--------------------------------
+-- FOREACH implementation :
+--------------------------------
+DECLARE @PrevNumb int;
+DECLARE @CurrentNumb int;
+DECLARE @Text varchar(30);
+
+SELECT TOP 1 @CurrentNumb = UserId, @Text = UserName FROM dbo.TableForTests 
+	ORDER BY UserId
+
+WHILE @CurrentNumb IS NOT NULL
+BEGIN
+	PRINT(CONCAT('Number=',@CurrentNumb,' Text=',@Text));
+
+	SELECT @PrevNumb = @CurrentNumb, @CurrentNumb = NULL, @Text = NULL;
+
+	SELECT TOP 1 @CurrentNumb = UserId, @Text = UserName 
+	FROM dbo.TableForTests 
+	WHERE @CurrentNumb > @PrevNumb
+	ORDER BY UserId
+END
+GO
--- Database1/Trigger_NoIntersectTime.sql
+++ Database1/Trigger_NoIntersectTime.sql
@@ -1,52 +1,48 @@
 ﻿ USE Database1_1
  GO
 
-DROP TABLE dbo.Table111
+DROP TABLE dbo.TableForTests
  
-IF OBJECT_ID('dbo.Table111') IS NULL
-CREATE TABLE dbo.Table111
+IF OBJECT_ID('dbo.TableForTests') IS NULL
+CREATE TABLE dbo.TableForTests
 (
 	Id INT NOT NULL IDENTITY PRIMARY KEY,
-	Name VARCHAR(30) NOT NULL,
+	UserName VARCHAR(30) NOT NULL,
 	StartedAt DATETIME NOT NULL,
 	MinutesDuration INT NOT NULL,
 	Comment VARCHAR(30)
 )
 GO
 
-DROP TRIGGER IF EXISTS dbo.TR_NoIntersectTimeSlot_Table111
+DROP TRIGGER IF EXISTS dbo.TR_NoIntersectTimeSlot_TableForTests
 GO
-CREATE TRIGGER TR_NoIntersectTimeSlot_Table111 on dbo.Table111 
+CREATE TRIGGER TR_NoIntersectTimeSlot_TableForTests on dbo.TableForTests 
 	INSTEAD OF INSERT
 AS
 BEGIN
 	IF NOT EXISTS (
-		-- SELECT 1 FROM dbo.Table1 e
-		-- WHERE EXISTS (
-		--		SELECT 1 FROM INSERTED i
-		--		WHERE ...
 		SELECT TOP 1 1 
-		FROM dbo.INSERTED as i
-        INNER JOIN dbo.Table111 AS e ON
-			i.StartedAt < DATEADD(MINUTE, e.MinutesDuration, e.StartedAt)
+		FROM INSERTED as i
+        INNER JOIN dbo.TableForTests AS e
+			ON i.StartedAt < DATEADD(MINUTE, e.MinutesDuration, e.StartedAt)
 			AND e.StartedAt < DATEADD(MINUTE, i.MinutesDuration, i.StartedAt)
 			AND e.Comment = 'Ok'
 		)
 	BEGIN
-		INSERT INTO dbo.Table111
-		(Name, StartedAt, MinutesDuration, Comment)
-		SELECT Name, StartedAt, MinutesDuration, 'Ok'
-		FROM dbo.INSERTED
+		INSERT INTO dbo.TableForTests
+		(UserName, StartedAt, MinutesDuration, Comment)
+		SELECT UserName, StartedAt, MinutesDuration, 'Ok'
+		FROM INSERTED
 	END
 	
 	ELSE
 	BEGIN
-		INSERT INTO dbo.Table111
-		(Name, StartedAt, MinutesDuration, Comment)
-		SELECT Name, StartedAt, MinutesDuration, 'Wrong!!!'
-		FROM dbo.INSERTED
+		INSERT INTO dbo.TableForTests
+		(UserName, StartedAt, MinutesDuration, Comment)
+		SELECT UserName, StartedAt, MinutesDuration, 'Wrong!!!'
+		FROM INSERTED
 		-- OR --
-		--RAISERROR('Time slot intersect with some other.', 16, 1)
+		-- RAISERROR('Time slot intersect with some other.', 16, 1)
 	END
 END
 GO
@@ -54,59 +50,59 @@ GO
 -- TEST
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Alex', @MyDate, 30)
 GO
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Felix', DATEADD(MINUTE, 10, @MyDate), 30)
 GO
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Maxx', DATEADD(MINUTE, 30, @MyDate), 30)
 GO
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Trisha', DATEADD(MINUTE, 65, @MyDate), 30)
 GO
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Sandra', DATEADD(MINUTE, 30, @MyDate), 30)
 GO
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 11:00:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Daisy', DATEADD(MINUTE, 65, @MyDate), 30)
 GO
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 12:29:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Catana', DATEADD(MINUTE, 5, @MyDate), 30)
 GO
 ----------------------------------------------------
 DECLARE @MyDate DATETIME = '2022-09-28 12:31:00';
-INSERT INTO dbo.Table111
-(Name, StartedAt, MinutesDuration)
+INSERT INTO dbo.TableForTests
+(UserName, StartedAt, MinutesDuration)
 VALUES
 ('Donald', DATEADD(MINUTE, 5, @MyDate), 30)
 GO
 ----------------------------------------------------
-SELECT * FROM dbo.Table111 ORDER BY StartedAt
+SELECT * FROM dbo.TableForTests ORDER BY StartedAt
