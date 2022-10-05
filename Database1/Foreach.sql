6a66e475ff0dad0597e5ee8dc0165374c5401a08
Hereigo <hereigo@online.ua>
2022-10-05 18:24:59 +0300

    sql foreach fix

--- Database1/Foreach.sql
+++ Database1/Foreach.sql
@@ -2,7 +2,7 @@
 GO
 
 DROP TABLE dbo.TableForTests
- 
+
 CREATE TABLE dbo.TableForTests
 (
 	UserId INT NOT NULL IDENTITY PRIMARY KEY,
@@ -14,26 +14,30 @@ INSERT INTO dbo.TableForTests (UserName)
 VALUES
 ('Alex'),('Felix'),('Max'),('Sandy'),('Daisy'),('Catana')
 GO
--- SELECT * FROM dbo.TableForTests
+
 --------------------------------
 -- FOREACH implementation :
 --------------------------------
-DECLARE @PrevNumb int;
-DECLARE @CurrentNumb int;
-DECLARE @Text varchar(30);
 
-SELECT TOP 1 @CurrentNumb = UserId, @Text = UserName FROM dbo.TableForTests 
-	ORDER BY UserId
+DECLARE @PrevNum int;
+DECLARE @CurrentNum int;
+DECLARE @User varchar(30);
 
-WHILE @CurrentNumb IS NOT NULL
+SELECT TOP 1 @CurrentNum = UserId, @User = UserName 
+FROM dbo.TableForTests 
+ORDER BY UserId
+
+WHILE @CurrentNum IS NOT NULL
 BEGIN
-	PRINT(CONCAT('Number=',@CurrentNumb,' Text=',@Text));
+	PRINT(CONCAT('User: ', @User, ' has ID = ', @CurrentNum))
 
-	SELECT @PrevNumb = @CurrentNumb, @CurrentNumb = NULL, @Text = NULL;
-
-	SELECT TOP 1 @CurrentNumb = UserId, @Text = UserName 
+	SELECT 
+		@PrevNum = @CurrentNum,
+		@User = NULL,
+		@CurrentNum = NULL
+		
+	SELECT TOP 1 @CurrentNum = UserId, @User = UserName
 	FROM dbo.TableForTests 
-	WHERE @CurrentNumb > @PrevNumb
+	WHERE UserId > @PrevNum
 	ORDER BY UserId
 END
-GO
